6ace6d541846484956a5a42c6e16416f289ab6d8
pages <146533303+lmsabc@users.noreply.github.com>
2025-06-17 11:00:27 +0800

    Add files via upload

---
+++ rag-project/构建RAG应用/streamlit_app.py
@@ -0,0 +1,132 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+import os
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableBranch, RunnablePassthrough
+import sys
+sys.path.append("搭建知识库") # 将父目录放入系统路径中
+# 获取当前文件所在目录的上级路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, "../搭建知识库")
+
+# 添加到系统路径
+sys.path.append(parent_dir)
+from zhipuai_embedding import ZhipuAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from zhipuai_llm import ZhipuaiLLM
+from dotenv import find_dotenv, load_dotenv
+import os
+
+# 读取本地/项目的环境变量。
+
+# find_dotenv()寻找并定位.env文件的路径
+# load_dotenv()读取该.env文件，并将其中的环境变量加载到当前的运行环境中
+# 如果你设置的是全局的环境变量，这行代码则没有任何作用。
+_ = load_dotenv(find_dotenv())
+
+# 获取环境变量 API_KEY
+api_key = os.environ["ZHIPUAI_API_KEY"] #填写控制台中获取的 APIKey 信息
+
+def get_retriever():
+    # 定义 Embeddings
+    embedding = ZhipuAIEmbeddings()
+    # 向量数据库持久化路径
+    persist_directory = '../data_base/vector_db/chroma'
+    # 加载数据库
+    vectordb = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embedding
+    )
+    return vectordb.as_retriever()
+
+def combine_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs["context"])
+
+def get_qa_history_chain():
+    retriever = get_retriever()
+    llm = ZhipuaiLLM(model_name="glm-4-plus", temperature=0.1, api_key=api_key)
+    condense_question_system_template = (
+        "请根据聊天记录总结用户最近的问题，"
+        "如果没有多余的聊天记录则返回用户的问题。"
+    )
+    condense_question_prompt = ChatPromptTemplate([
+            ("system", condense_question_system_template),
+            ("placeholder", "{chat_history}"),
+            ("human", "{input}"),
+        ])
+
+    retrieve_docs = RunnableBranch(
+        (lambda x: not x.get("chat_history", False), (lambda x: x["input"]) | retriever, ),
+        condense_question_prompt | llm | StrOutputParser() | retriever,
+    )
+
+    system_prompt = (
+        "你是一个问答任务的助手。 "
+        "请使用检索到的上下文片段回答这个问题。 "
+        "如果你不知道答案就说不知道。 "
+        "请使用简洁的话语回答用户。"
+        "\n\n"
+        "{context}"
+    )
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("placeholder", "{chat_history}"),
+            ("human", "{input}"),
+        ]
+    )
+    qa_chain = (
+        RunnablePassthrough().assign(context=combine_docs)
+        | qa_prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    qa_history_chain = RunnablePassthrough().assign(
+        context = retrieve_docs, 
+        ).assign(answer=qa_chain)
+    return qa_history_chain
+
+def gen_response(chain, input, chat_history):
+    response = chain.stream({
+        "input": input,
+        "chat_history": chat_history
+    })
+    for res in response:
+        if "answer" in res.keys():
+            yield res["answer"]
+
+# Streamlit 应用程序界面
+def main():
+    st.markdown('### 🦜🔗 动手学大模型应用开发')
+
+    # 用于跟踪对话历史
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    # 存储检索问答链
+    if "qa_history_chain" not in st.session_state:
+        st.session_state.qa_history_chain = get_qa_history_chain()
+    messages = st.container(height=550)
+    # 显示整个对话历史
+    for message in st.session_state.messages:
+            with messages.chat_message(message[0]):
+                st.write(message[1])
+    if prompt := st.chat_input("Say something"):
+        # 将用户输入添加到对话历史中
+        st.session_state.messages.append(("human", prompt))
+        with messages.chat_message("human"):
+            st.write(prompt)
+
+        answer = gen_response(
+            chain=st.session_state.qa_history_chain,
+            input=prompt,
+            chat_history=st.session_state.messages
+        )
+        with messages.chat_message("ai"):
+            output = st.write_stream(answer)
+        st.session_state.messages.append(("ai", output))
+
+
+if __name__ == "__main__":
+    main()
